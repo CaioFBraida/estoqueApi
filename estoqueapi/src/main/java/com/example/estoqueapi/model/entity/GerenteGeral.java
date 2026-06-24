d9f3e48bbb9c676c2bdb3e917a6acf054d81130d
ze-miguel <malikjmiguel0@gmail.com>
2026-06-24 18:46:42 -0300

    subida de erro javax.persistence

---
+++ estoqueapi/src/main/java/com/example/estoqueapi/model/entity/GerenteGeral.java
@@ -0,0 +1,15 @@
+package com.example.estoqueapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GerenteGeral extends Usuario {
+
+}
